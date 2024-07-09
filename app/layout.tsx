4cec5f8c02f880000d25c8071809da64906176f0
Martin van Amersfoorth <martin.van.amersfoorth@finalist.nl>
2024-07-09 17:36:03 +0200

    Restyle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,33 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import './globals.css'
+import { Raleway } from 'next/font/google'
+
+const raleway = Raleway({
+  weight: '400',
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000'
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+  title: 'Rad RAG',
+  description: 'RAG app demo',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang='en' data-theme='finalist'>
+      <body className={raleway.className}>
+        <div className='p-4'>{children}</div>
       </body>
     </html>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,27 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        finalist: {
+          primary: '#ff9100',
+          'primary-content': '#000',
+          secondary: '#faefe7',
+          'secondary-content': '#000',
+          accent: '#c0c0ff',
+          'accent-content': '#000',
+          neutral: '#162020',
+          'neutral-content': '#fff',
+          'base-100': '#ffffff',
+          'base-100-content': '#000',
+          'base-200': '#dfe5e5',
+          'base-200-content': '#000',
+          'base-300': '#c1cece',
+          'base-300-content': '#fff',
         },
       },
-    },
+    ],
   },
-  plugins: [],
 };
