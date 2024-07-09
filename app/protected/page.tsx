542a13809325908dd9e3138b43ce7d21ea562a11
Martin van Amersfoorth <martin.van.amersfoorth@finalist.nl>
2024-07-09 20:03:52 +0200

    Add protected page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,27 +1,3 @@
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
-import AuthButton from '@/components/AuthButton'
-
 export default async function ProtectedPage() {
-  const supabase = createClient()
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  if (!user) {
-    return redirect('/login')
-  }
-
-  return (
-    <div className='p-4'>
-      <nav className='flex justify-between w-full'>
-        <div className='text-xl font-semibold'>Rad RAG</div>
-        <div className=''>
-          <AuthButton />
-        </div>
-      </nav>
-      <div>Protected</div>
-    </div>
-  )
+  return <div>Protected</div>
 }
